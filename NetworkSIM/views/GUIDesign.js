e499bce10b1832356783376ef5d03153146630a7
Noah <Noah@192.168.0.10>
2015-02-12 11:24:12 -0330

    working on a gui mockup, networks can be moved now

--- NetworkSIM/views/GUIDesign.js
+++ NetworkSIM/views/GUIDesign.js
@@ -11,16 +11,49 @@ var svgCanvas = document.querySelector('svg'),
  * @param radius: radius of the server
  * @param svgCanvas: the canvas to paint this object on
  */
-function circle(centerX, centerY, radius, svgCanvas, circleClass){
+function circle(centerX, centerY, radius, svgCanvas){
 	this.x=centerX;
 	this.y=centerY;
 	this.r=radius;
-	this.stroke=2;
+	this.stroke=1;
 	
 	//creates an SVG element in the DOM as a circle
 	this.element = document.createElementNS(svgNS, 'circle');
+	//set the unique id of this shape
+	this.element.setAttribute('data-index', shapes.length);
 	//sets the class of this shape to 
-	this.el.setAttribute('class', circleClass);
-	shapes.push(this;)
+	this.element.setAttribute('class', 'network');
+	shapes.push(this);
+	
+	this.draw();
+	//adds the circle to the svg canvas
+	svgCanvas.appendChild(this.element);
 }
 
+circle.prototype.draw=function(){
+	//should it have something to do with stroke?
+	this.element.setAttribute('cx', this.x); 
+	this.element.setAttribute('cy', this.y);
+	this.element.setAttribute('r', this.r);
+	this.element.setAttribute('stroke', this.stroke);
+}
+
+//listener for the events for the ".network" class
+interact('.network')
+//handles a dragging event for the circle
+.draggable({
+  max: Infinity,
+  //handles moving the circle
+  onmove: function (event) {
+	//gets the circle from the list of shapes
+    var rectangle = shapes[event.target.getAttribute('data-index')];
+
+    rectangle.x += event.dx;
+    rectangle.y += event.dy;
+    rectangle.draw();
+  }
+})
+
+for (var i = 0; i < 5; i++) {
+  new circle(50 + 100 * i, 80, 80, svgCanvas);
+}
