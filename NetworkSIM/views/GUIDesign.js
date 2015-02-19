d9d0daefbfd5207efda1f1c085d267a5897dd66b
Emily-Innes <emily.innes@live.com>
2015-02-18 22:52:51 -0330

    Noah please check that the bugs are gone

--- NetworkSIM/views/GUIDesign.js
+++ NetworkSIM/views/GUIDesign.js
@@ -1,93 +1,49 @@
+// We are using uniqueDataIndex to track shapes. It only ever increases.
+ 
 var uniqueDataIndex=0;
 var svgCanvas = document.querySelector('svg'),
 
-//gets SVG in order to display graphics
+//svg is required for the graphics
     svgNS = 'http://www.w3.org/2000/svg',
     shapes=[];
+
 var origin = {x: 0, y: 0};
 var mouse = {x: 0, y: 0};
 
-document.addEventListener('mousemove', function(e){ 
-    mouse.x = e.clientX || e.pageX; 
-    mouse.y = e.clientY || e.pageY 
-}, false);
 
 
 /****
- * -------------------
- * Shape Classes
- * -------------------
- ****/
-
-/****
- * Circle object to be rendered by SVG.
- * @param svgCanvas: the canvas to paint this object on
- * @param elementClass: The class of the circle object to be created, used to attach CSS.
- ****/
-function circle(centerX, centerY, radius, svgCanvas, elementClass){
-	this.x=centerX;
-	this.y=centerY;
-	this.r=radius;
-	this.stroke=1;
-	
-	//creates an SVG element in the DOM as a circle
-	this.element = document.createElementNS(svgNS, 'circle');
-	//set the unique id of this shape
-	this.element.setAttribute('data-index', uniqueDataIndex);
-	this.element.setAttribute('class', elementClass);
-	this.children=[];
-	this.connections=[];
-	this.draw();
-	//adds the circle to the svg canvas
-	svgCanvas.appendChild(this.element);
+ * The main functions used in our GUI
+ ***/
+function createNetwork(){
+	var  network=new circle(100, 500, 80, svgCanvas, 'network');
+	shapes[uniqueDataIndex]=(network);
+	uniqueDataIndex++;
 }
 
-circle.prototype.draw=function(){
-	this.element.setAttribute('cx', this.x); 
-	this.element.setAttribute('cy', this.y);
-	this.element.setAttribute('r', this.r);
-	this.element.setAttribute('stroke', this.stroke);
-	//orders the canvas so that devices are above networks
-	orderCanvas();
+
+function createDevice(){
+	var device=new circle(50, 50, 10, svgCanvas, 'device');
+	shapes[uniqueDataIndex]=(device);
+	uniqueDataIndex++;
 }
 
-/****
- * Line object to be rendered by SVG.
- * @param svgCanvas: the canvas to paint this object on
- * @param elementClass: The class of the line object to be created, used to attach CSS.
- ****/
-function line(x1, y1, x2, y2, svgCanvas, elementClass){
-	this.x1=x1;
-	this.x2=x2;
-	this.y1=y1;
-	this.y2=y2;
-	this.stroke=4
-	
-	this.element = document.createElementNS(svgNS, 'line');
-	//set the unique id of this shape
-	this.element.setAttribute('data-index', uniqueDataIndex);
-	//sets the class of this shape to 
-	this.element.setAttribute('class', elementClass);
-	
-	this.draw();
-	//adds the line to the svg canvas
-	svgCanvas.appendChild(this.element);
+function createPartition(sourceNetwork, destinationNetwork){
+	var connection=new line(origin.x,origin.y,destinationNetwork.x, destinationNetwork.y,svgCanvas,'network-connection');
+	shapes[uniqueDataIndex]=(connection);
+	sourceNetwork.connections[uniqueDataIndex]=destinationNetwork;
+	destinationNetwork.connections[uniqueDataIndex]=sourceNetwork;
+	uniqueDataIndex++;
+}
+function removePartition(sourceNetwork,destinationNetwork){
+	var index=sourceNetwork.connections.indexOf(destinationNetwork);
+	delete destinationNetwork.connections[index];
+	delete sourceNetwork.connections[index];
+	var oldLine=shapes[index];
+	svgCanvas.removeChild(oldLine.element);
+	delete shapes[index];
 }
 
-line.prototype.draw=function(){
-	this.element.setAttribute('x1', this.x1);
-	this.element.setAttribute('x2', this.x2);
-	this.element.setAttribute('y1', this.y1);
-	this.element.setAttribute('y2', this.y2);
-	this.element.setAttribute('stroke-width', this.stroke);
-	orderCanvas();
-}
-line.prototype.update=function(x1,y1,x2,y2){
-	this.x1=x1;
-	this.y1=y1;
-	this.x2=x2;
-	this.y2=y2;
-}
 /****
  * --------------
  * Object Interactions
@@ -111,11 +67,7 @@ interact('.device')
 		
 		//gets the circle from the list of shapes
 			var circle = getShapeFromEvent(event);
-			
-			//updates the location
-			circle.x += event.dx;
-			circle.y += event.dy;
-			circle.draw();
+			moveUIElementAndChildren(circle, event.dx,event.dy);
 		}
 	});
 
@@ -166,8 +118,8 @@ interact('.network')
 
 		
 		ondrop: function (event) {
-			var draggableElement = event.relatedTarget;
-	        var dropzoneElement = event.target;
+			var draggableElement=event.relatedTarget;
+			var dropzoneElement=event.target;
 			var dragClass=draggableElement.classList[0];
 			
 			var dropzone=getShapeFromEvent(event);
@@ -181,16 +133,13 @@ interact('.network')
 			}
 			if(dragClass==='network'){
 				
-				
 				if(!partitionExists(dropzone,dragged)){
 					var partition=createPartition(dropzone,dragged);
 				}
 				else{
 					removePartition(dropzone,dragged);
 				}
-				var networkIndex=draggableElement.getAttribute('data-index');
-				var heldCircle=shapes[networkIndex];
-				snapToLocation(heldCircle,origin);
+				snapToLocation(dragged,origin);
 			}
 		
 		},
@@ -211,22 +160,13 @@ interact('.network')
 		    },
 		
 			onmove: function (event) {
-				
-				var circle = shapes[event.target.getAttribute('data-index')];
-				circle.x += event.dx;
-				circle.y += event.dy;
-				for(index in circle.children){
-					circle.children[index].x+=event.dx;
-					circle.children[index].y+=event.dy;
-					circle.children[index].draw();
-				}
+				var circle = getShapeFromEvent(event);
+				moveUIElementAndChildren(circle,event.dx,event.dy);
 				updatePartitionLines(circle);
-				circle.draw();
 			},
 			
 			onstart: function(event){
-		    	
-		    	var circle = shapes[event.target.getAttribute('data-index')];
+		    	var circle = getShapeFromEvent(event);
 				origin.x=circle.x;
 				origin.y=circle.y;
 				
@@ -235,12 +175,12 @@ interact('.network')
 		
 			
 		});
-	
-/**
+
+/****
  * --------
  * Utilities and Button Handlers
  * -----------------
- */
+ ****/
 
 function snapToLocation(shape,coordinates){
 	
@@ -258,6 +198,7 @@ function snapToLocation(shape,coordinates){
 	updatePartitionLines(shape);
 
 	shape.draw();
+	
 }
 function updatePartitionLines(networkShape){
 	for(index in networkShape.connections){
@@ -269,9 +210,17 @@ function updatePartitionLines(networkShape){
 	}
 	
 }
-/****
-* We'll update this later so that it actually checks if the partition doesn't already exist.
- ****/
+function moveUIElementAndChildren(UIShape,dx,dy){
+	UIShape.x +=dx;
+	UIShape.y += dy;
+	for(index in UIShape.children){
+		UIShape.children[index].x+=dx;
+		UIShape.children[index].y+=dy;
+		UIShape.children[index].draw();
+	}
+	UIShape.draw();
+}
+
 function partitionExists(dropzoneObject, dragObject){
 	var index=dropzoneObject.connections.indexOf(dragObject);
 	return index>-1
@@ -285,9 +234,13 @@ function getRelatedShapeFromEvent(event){
 	var index=event.relatedTarget.getAttribute('data-index');
 	return shapes[index];
 }
+document.addEventListener('mousemove', function(e){ 
+    mouse.x = e.clientX || e.pageX; 
+    mouse.y = e.clientY || e.pageY 
+}, false);
 /****
  * orderCanvas orders all the shapes on the canvas so that devices
- * appear in front of networks.
+ * appear in front of networks, and so that connection lines appear behind the networks
  ****/
 function orderCanvas(){
 	for (index in shapes){
@@ -301,23 +254,9 @@ function orderCanvas(){
 		}
 	}
 }
-
-
-function createNetwork(){
-	var  network=new circle(100, 500, 80, svgCanvas, 'network');
-	shapes[uniqueDataIndex]=(network);
-	uniqueDataIndex++;
-}
-
-
-function createDevice(){
-	var device=new circle(50, 50, 10, svgCanvas, 'device');
-	shapes[uniqueDataIndex]=(device);
-	uniqueDataIndex++;
-}
-/***
+/****
  * handles getting what the mouse has moved over 
- */
+ ****/
 function mouseOver(e){
 	e = e || event;
 	if (event.type == 'mouseover'){
@@ -332,18 +271,74 @@ function mouseOver(e){
 }
 document.body.onmouseover = document.body.onmouseout = mouseOver;
 
-function createPartition(sourceNetwork, destinationNetwork){
-	var connection=new line(origin.x,origin.y,destinationNetwork.x, destinationNetwork.y,svgCanvas,'network-connection');
-	shapes[uniqueDataIndex]=(connection);
-	sourceNetwork.connections[uniqueDataIndex]=destinationNetwork;
-	destinationNetwork.connections[uniqueDataIndex]=sourceNetwork;
-	uniqueDataIndex++;
+
+/****
+ * -------------------
+ * Shape Classes
+ * -------------------
+ ****/
+
+/****
+ * Circle object to be rendered by SVG.
+ * @param svgCanvas: the canvas to paint this object on
+ * @param elementClass: The class of the circle object to be created, used to attach CSS.
+ ****/
+function circle(centerX, centerY, radius, svgCanvas, elementClass){
+	this.x=centerX;
+	this.y=centerY;
+	this.r=radius;
+	this.stroke=1;
+	
+	this.element = document.createElementNS(svgNS, 'circle');
+	this.element.setAttribute('data-index', uniqueDataIndex);
+	this.element.setAttribute('class', elementClass);
+	this.children=[];
+	this.connections=[];
+	
+	this.draw();
+	svgCanvas.appendChild(this.element);
 }
-function removePartition(sourceNetwork,destinationNetwork){
-	var index=sourceNetwork.connections.indexOf(destinationNetwork);
-	destinationNetwork.connections.splice(index,1);
-	sourceNetwork.connections.splice(index,1);
-	var oldLine=shapes[index];
-	svgCanvas.removeChild(oldLine.element);
-	shapes.splice(index,1);
+
+circle.prototype.draw=function(){
+	this.element.setAttribute('cx', this.x); 
+	this.element.setAttribute('cy', this.y);
+	this.element.setAttribute('r', this.r);
+	this.element.setAttribute('stroke', this.stroke);
+
+	orderCanvas();
+}
+
+/****
+ * Line object to be rendered by SVG.
+ * @param svgCanvas: the canvas to paint this object on
+ * @param elementClass: The class of the line object to be created, used to attach CSS.
+ ****/
+function line(x1, y1, x2, y2, svgCanvas, elementClass){
+	this.x1=x1;
+	this.x2=x2;
+	this.y1=y1;
+	this.y2=y2;
+	this.stroke=4
+	
+	this.element = document.createElementNS(svgNS, 'line');
+	this.element.setAttribute('data-index', uniqueDataIndex);
+	this.element.setAttribute('class', elementClass);
+	
+	this.draw();
+	svgCanvas.appendChild(this.element);
+}
+
+line.prototype.draw=function(){
+	this.element.setAttribute('x1', this.x1);
+	this.element.setAttribute('x2', this.x2);
+	this.element.setAttribute('y1', this.y1);
+	this.element.setAttribute('y2', this.y2);
+	this.element.setAttribute('stroke-width', this.stroke);
+	orderCanvas();
+}
+line.prototype.update=function(x1,y1,x2,y2){
+	this.x1=x1;
+	this.y1=y1;
+	this.x2=x2;
+	this.y2=y2;
 }
