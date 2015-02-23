afb74cabeb0bbd5edf66cc5f8e78f158c925bc62
Noah <Noah@192.168.0.10>
2015-02-22 20:55:11 -0330

    working

--- NetworkSIM/views/EventLogView.js
+++ NetworkSIM/views/EventLogView.js
@@ -1,5 +1,8 @@
 var templateLog="01/02/2014";//put some log here for testing
 
+var stateList={};
+var timeStampList=[];
+
 /**
  * Updates the title of the page with the name of the simulation
  */
@@ -30,10 +33,10 @@ function populateDates(dates){
  * Adds the list of all events that have occurred on this simulation up to this point
  * NOTE: should probably be reverse-chronological order
  */
-function populateSimulationLogs(simulationEvents){
+function populateSimulationLogs(simulationDates){
 	var simulationLogs= document.getElementById("simulation-log");
 	simulationLogs.innerHTML="";
-	for (event in simulationEvents){
+	for (event in simulationDates){
 		simulationLogs.innerHTML+="<option value="+event+">"+event+"</option>";
 	}
 }
@@ -52,9 +55,43 @@ function populateDeviceLogs(deviceEvents){
  * function which is called when the user selects a date to view
  * that simulation state from
  */
-function selectSimulationDate(event){
+function selectSimulationDate(selected){
 	//should render the current simulation in the simulation thingy
 	//look through the passed in list of all the shit, find the 
 	//correct date, render the simulation from there.
-	alert(event);
+	console.log(selected);//event should be the time at which this simulation is
+	//gets the configMap for that time stamp
+	configMap=configMapList[event];
+	
+}
+
+function updateSimulationEvents(time){
+	state=stateList[time];
+	state.devices
+}
+
+
+//states.id.activity_logs;
+//localSession holds the current session and all events that have happened up to this point
+function parseStateList(statesObject){
+	//list of config maps as key value pairs indexed by dates
+	var states= statesObject.states;
+	for (var i =0; i<states.length;i++){
+		//HERE WE ARE ASSUMING THAT TIMESTAMP IS A STRING
+		
+		time=states[i].timeStamp; //timestamp for when that state occured
+		//configMap=states.id.config_map; //gets the config map for that state
+		
+		//add the timestamp to the list of timestamps
+		timeStampList.push(time);
+		
+		stateList[time]=states;
+	}
+}
+
+device.activity
+
+function loadPage(statesObject){
+	parseStateList(statesObject);
+	populateSimulationLogs(timeStampList);
 }
