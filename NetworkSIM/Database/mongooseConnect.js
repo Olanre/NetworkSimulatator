e45127106f49c947039b9fb0982b14a9cddf4648
cs <cs@checker>
2015-02-23 19:23:30 -0330

    State working

--- NetworkSIM/Database/mongooseConnect.js
+++ NetworkSIM/Database/mongooseConnect.js
@@ -60,25 +60,30 @@ var simulationSchema = mongoose.Schema({
 	var Sim = mongoose.model('Sim', simulationSchema, 'newSimFormat');
 
 //STATE - storing logs as a string
-/** var stateSchema = mongoose.Schema({
+ var stateSchema = mongoose.Schema({
 	id : String,
-	state : [{ id : Sim, 
-			  timeStamp : String, 
-			  devices : [ {device : User} ] ,
-	}]
+	state : [StateObject],
 });
 
 //State complete
 var State = mongoose.model('State', stateSchema, ' newStateSchema');
 
-*/
+
+//State building block
+var stateObject = mongoose.Schema({
+   id : Sim,
+   timeStamp : String,
+   devices : [User],
+});
+//state building block complete
+var StateObject = mongoose.model('StateObject', stateObject, 'newStateObject');
+
+
 var partitionSchema = mongoose.Schema({
 	partition_name: String,
 	network_list: [Network]
 
 });
-
-
 
 var Partition = mongoose.model('Partition', partitionSchema, 'newSimFormat');
 //SIMULATION
