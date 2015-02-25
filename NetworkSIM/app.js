af539dd69605008499c7e62e3f5955d6f754e115
Noah <Noah@10.23.12.53>
2015-02-25 16:00:07 -0330

    errors on excalibur

--- NetworkSIM/app.js
+++ NetworkSIM/app.js
@@ -17,11 +17,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 //app.use(express.favicon());
 //app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(app.router);
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
