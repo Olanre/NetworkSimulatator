ba0c4d1c181d72488ee59476476a5868442f6450
Olanrewaju Okunlola <ooo524@mun.ca>
2015-03-02 11:33:48 -0330

    some more refactoring

--- NetworkSIM/public/ClientJS/Database Models/DBModel.js
+++ NetworkSIM/public/ClientJS/Database Models/DBModel.js
@@ -18,14 +18,14 @@ function get_local_device(){
  * @reutrn, a JSON representation of the object in storage
  */
 function get_local_session(){
-	return JSON.parse(getfromStorage('session'));
+	return JSON.parse(getfromStorage('simulation'));
 }
 
 /** Function to get the current application from local database
  * @reutrn, a JSON representation of the object in storage
  */
 function get_local_application(){
-	return JSON.parse(getfromStorage('application'));
+	return JSON.parse(getfromStorage('simulation_names'));
 }
 
 /** Function to get the current app state from local database
@@ -40,7 +40,7 @@ function get_local_appstate(){
  * @returns
  */
 function get_local_states(){
-	return JSON.parse(getfromStorage('states'));
+	return JSON.parse(getfromStorage('simulation_states'));
 }  /*****  ... to line 2413  *****/
 
 /** ** from line 2415 to ... ** 
@@ -59,73 +59,12 @@ function get_local_events(){
  * ----------------------------------------
  */
 
-function updateCurrentPartition(new_partition){
-	var local_device = get_local_device();
-	local_device.current_partition = new_network;
-	putinStorage( 'device', JSON.stringify(local_device) );
-}
-
-
-function updateCurrentNetwork(new_network){
-	var local_device = get_local_device();
-	local_device.current_network = new_network;
-	putinStorage( 'device', JSON.stringify(local_device) );
-	
-}
-
-function updateCurrentSimulation(new_simulation){
-	var local_device = get_local_device();
-	local_device.current_simulation = new_simulation;
-	putinStorage( 'device', JSON.stringify(local_device) );
-	
-}
-
-
-function updateRegisteredIn(new_list){
-	var local_device = get_local_device();
-	local_device.registeredIn = new_list;
-	putinStorage( 'device', JSON.stringify(local_device) );
-}
-
-
-function updateLocalEventsToken(token){
-	//gets the events
-	var local_events = get_local_events();
-	//sets the token to the token of the user
-	local_events.token = token;
-	//returns the new object to the local storage.
-	putinStorage( 'localevents', JSON.stringify(local_events) );
-}
 
 
 function updateDeviceName(old_name, new_name){
 	var local_device = get_local_device();
 	var local_session = get_local_session();
-	var partition = getPartitionfromDevice( old_name);
-	var network = getNetwork(old_name);
-	
-	//rename it in the local device
-	if(local_device.current_device_name == old_name){ 
-		local_device.current_device_name == new_name;
-	}
-	
-	//renae it in config map nested object if necessary
-	if(network !== null && partition !== null){
-		local_session.config_map[partition][network][new_name] = local_session.config_map[partition][network][old_name]
-		delete local_session.config_map[partition][network][old_name];
-	}
-	
-	//rename it in free list object
-	var list = local_session.config_map['freelist'];
-	if( list.hasOwnProperty(device_name) ){
-		local_session.config_map['freelist'][new_name] = local_session.config_map['freelist'][old_name];
-		delete local_session.config_map['freelist'][old_name];
-	}
-	
-	
-	putinStorage( 'device', JSON.stringify(local_device) );
-	putinStorage( 'session', JSON.stringify(local_session) );
-	
+
 	//send to event queue
 	var params = { 
 			'config_map' : local_session.config_map,
@@ -142,30 +81,6 @@ function updateDeviceName(old_name, new_name){
 function updateNetworkName(old_name, new_name){
 	var local_device = get_local_device();
 	var local_session = get_local_session();
-	var partition = getPartition(old_name);
-	//check if the name is the network name of the current device
-	if(local_device.current_network == old_name){
-		//if so change it
-		local_device.current_network = new_name;
-	}
-	
-	//update in list of networks created
-	var list = local_device.networks_created;
-	for(var i = 0; i < list.length; i++){
-		if( local_device.networks_created[i] == old_name){
-			local_device.networks_created[i] = new_name;
-		}
-	}
-	
-	//perform the renaming of the network in configuration map
-	var temp = local_session.config_map[Partition_name][old_name];
-	
-	local_session.config_map[Partition_name][new_name] = temp;
-	//deletes the network
-	delete local_session.config_map[Partition_name][old_name];
-
-	pputinStorage( 'device', JSON.stringify(local_device) );
-	putinStorage( 'session', JSON.stringify(local_session) );
 	
 	//send to event queue
 	var params = { 
@@ -182,29 +97,7 @@ function updateNetworkName(old_name, new_name){
 function updateSimulationName(old_name, new_name){
 	var local_application = get_local_application();
 	var local_session = get_local_session();
-	var local_device = get_local_device();
-	
-	
-	if( old_name == local_session.simulation_name){
-		local_session.simulation_name = new_name;
-	}
-	var list = local_application.simulation_list;
-	//iterate over the list, when we find the current simulation reduce the number of networks by 1
-	for(var i = 0; i < list.length; i++ ){
-		if( local_application.simulation_list[i].name == old_name){
-			local_application.simulation_list[i].name = new_name;
-			break;
-		}
-	}
-	
-	if(local_device.current_simulation == old_name){
-		local_device.current_simulation == new_name;
-	}
-	
-	putinStorage( 'device', JSON.stringify(local_device) );
-	putinStorage( 'session', JSON.stringify(local_session) );
-	putinStorage( 'application', JSON.stringify(local_application) );
-	
+
 	//send to event queue
 	var params = { 
 			'config_map' : local_session.config_map,
@@ -217,59 +110,8 @@ function updateSimulationName(old_name, new_name){
 	addToEventQueue(url, params, timestamp);
 }
 
-function updateDeviceNumber(new_number){
-	var local_session = get_local_session();
-	var local_application = get_local_application();
-	var sim = local_session.simulation_name;
-	local_application.total_devices = new_number;
-	local_session.num_devices = new_number;
-	var list = local_application.simulation_list;
-	
-	for(var i = 0; i < list.length; i++ ){
-		if( local_application.simulation_list[i].name == sim){
-			local_application.simulation_list[i].num_devices = new_number; 
-		}
-	}
-	
-	var params = { 
-			'config_map' : local_session.config_map,
-			'device_number': new_number, 
-			'simulation_name': local_session.simulation_name,
-			};
-	var url = '/update/DeviceNumber';
-	var timestamp = new Date();
-	addToEventQueue(url, params, timestamp);
-	
-	putinStorage( 'application', JSON.stringify(local_application) );
-	putinStorage( 'session', JSON.stringify(local_session) );
 
-}
 
-function updateNetworkNumber(new_number){
-	var local_session = get_local_session();
-	var local_application = get_local_application();
-	var sim = local_session.simulation_name;
-	local_application.total_networks = new_number;
-	local_session.num_networks = new_number;
-	var list = local_application.simulation_list;
-	for(var i = 0; i < list.length; i++ ){
-		if( local_application.simulation_list[i].name == sim){
-			local_application.simulation_list[i].num_networks = new_number; 
-		}
-	}
-	var params = { 
-			'config_map' : local_session.config_map,
-			'network_number': new_number, 
-			'simulation_name': local_session.simulation_name,
-			};
-	var url = '/update/NetworkNumber';
-	var timestamp = new Date();
-	addToEventQueue(url, params, timestamp);
-	console.log(new_number);
-	console.log(local_session);
-	putinStorage( 'application', JSON.stringify(local_application) );
-	putinStorage( 'session', JSON.stringify(local_session) );
-}
 
 function updateLocalCount(){
 	var local_device = get_local_device();
@@ -285,60 +127,6 @@ function updateLocalCount(){
 	appDefaultView();
 }
 
-function updateUserLocalCount(new_count){
-	var local_device = get_local_device();
-	local_device.localcount += new_count;
-	//console.log(local_device.localcount);
-	putinStorage( 'device', JSON.stringify(local_device) );
-
-	
-}
-
-function updateUserGlobalCount(new_count){
-	var local_device = get_local_device();
-	local_device.globalcount += new_count;
-	putinStorage( 'device', JSON.stringify(local_device) );
-}
-
-
-function update_config_map(new_map){
-	var local_session = get_local_session();
-	local_session.config_map = new_map;
-	putinStorage( 'session', JSON.stringify(local_session) );
-}
-
-function updateSessionGlobalCount(new_count){
-	var local_session = get_local_session();
-	local_session.globalcount += new_count;
-	putinStorage( 'session', JSON.stringify(local_session) );
-	
-}
-
-function updateSimulationLog(new_comments){
-	var local_session = get_local_session();
-	local_session.activity_logs += new_comments;
-	putinStorage( 'session', JSON.stringify(local_session) );
-}
-
-function updateDeviceLog(new_activity){
-	local_device = get_local_device();
-	local_device.activity += new_activity;
-}
-
-
-function updateTokenMethod( new_method){
-	var local_session = get_local_session();
-	local_session.tokenMethod = new_method;
-	putinStorage( 'session', JSON.stringify(local_session) );
-}
-
-function updateAppState(){
-	appstate = get_local_appstate();
-	appstate.user = get_local_device();
-	appstate.current_simulation_session = get_local_session();
-	appstate.application = get_local_application();
-	putinStorage( 'appstate', JSON.stringify(appstate) );
-} /* ** .. to line 2710 ** */
 
 
 
