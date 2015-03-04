3657878c5feb1430597f68d1c543b6fd2f11a21a
Noah <Noah@10.23.14.95>
2015-03-04 09:55:07 -0330

    changing some schemas

--- NetworkSIM/Database/models/Simulation.js
+++ NetworkSIM/Database/models/Simulation.js
@@ -4,16 +4,12 @@ var Schema = mongoose.Schema;
 
 //Simulation
 var simulationSchema = mongoose.Schema({
-	  // partion_list : [Partition],
-	   //free_list : [User],
+	   partion_list : [Partition],
 	   num_devices: Number,
 	   num_networks: Number,
 	   simulation_population: Number,
 	   simulation_name: String,
-	   //config map isn't used anymore
-	   config_map: String,
 	   tokenMethod : String,
-	   //token_list : [tokens],
 	   activity_logs : String,
 	});
 
