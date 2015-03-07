9fe2647b7e8971d775361a941f9cfa4d8a419818
Olanre <Olanre@Olanre-PC>
2015-03-07 16:38:35 -0330

    moving to test branch

--- NetworkSIM/tests/SocketServer.js
+++ NetworkSIM/tests/SocketServer.js
@@ -15,6 +15,7 @@ var path = require('path');
 var logger = require('express-logger');
 var fs = require('fs');
 
+var SimulationManager = require("../Server//SimulationManager.js");
 //var Database = require("./Database/mongooseConnect.js");
 
 var globalCount = 0;
@@ -47,12 +48,99 @@ var handleClient = function (socket) {
          }
     });
     
-    socket.on("/getSync", Router.sync);
+    socket.on("/getSync", sync);
 
-    socket.on("/authenticate/authToken", Router.authToken );
+    socket.on("/authenticate/authToken", authToken );
     
     
 };
+
+function sync(data){
+	
+	var json = JSON.parse(data);
+	var token = json.token;
+	var events = json.eventQueue ;
+	var simulation= json.simulationId;
+	//console.log(obj);
+	SimulationManager.authToken(token, simulation, function(obj){
+		//for now allow empty tokens
+		if(obj.Response == 'Success'){
+			//console.log("Successful authenication" );
+				handleEventQueue(token, events, function(){
+
+				response.send(SimulationManager.getAppStateForDevice(token,simulation));
+			});
+			
+		}
+
+		else{
+			handleEventQueue(token, events, function(){
+
+				var state={};
+				state.simulation= {};
+				state.device= {};
+				state.simulation_names=SimulationManager.getSimulationNames();
+				response.send(state);
+				
+			});
+		}
+		
+	});
+}
+
+function authToken(data){
+	var json = JSON.parse(data);
+	var token = json.token;
+	var simulation_id = json.simulation_id;
+	SimulationManager.authToken(token, simulation_id, function(obj){
+	//for now allow empty tokens
+		console.log(obj);
+		response.send(obj);
+	});
+}
+
+function handleEventQueue(token, eventQueue, callback) {
+	for(var i = 0; i < eventQueue.length; i++) {
+		
+		switch(eventQueue[i].route) {
+
+
+			case '/create/Simulation': 
+				SimulationManager.createSimulation(eventQueue[i].event_data);
+				break;
+
+			case '/create/Network' :
+				SimulationManager.createNetwork( eventQueue[i].event_data);
+				break;		
+
+			case '/create/Device' :
+					SimulationManager.createDevice(eventQueue[i].event_data);
+				break;
+
+			case '/move/Device/Network':
+					SimulationManager.addDeviceToNetwork(eventQueue[i].event_data);
+				break;
+
+			case '/merge/Partitions' :
+				SimulationManager.mergePartitions(eventQueue[i].event_data);
+				break;
+			
+			case '/divide/Partition':
+				SimulationManager.dividePartition(eventQueue[i].event_data);
+				break;
+
+			default:
+				break;
+				
+		}	
+	}
+	
+	
+	if (typeof(callback) == "function") {
+		callback();
+	}
+};
+
  
 
 io.on("connection", handleClient);
@@ -62,13 +150,25 @@ server.listen(3000, function(){
 	});
 
 app.get('/', function(request,response){
-	response.sendFile("/index.html", {"root": __dirname});	
+	response.sendFile("./index.html", {"root": __dirname});	
 }); 
 
 app.get('/index', function(request,response){
-	response.sendFile("/index.html", {"root": __dirname});
+	response.sendFile("./index.html", {"root": __dirname});
 	
 }); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use("/css",  express.static(__dirname + '../public/stylesheets'));
+app.use("/logic", express.static(__dirname + '../public/ClientJS/ClientLogic'));
+app.use("/template", express.static(__dirname + '../public/ClientJS/HTML_Templates'));
+app.use("/view", express.static(__dirname + '../public/ClientJS/Views'));
+app.use("/gui", express.static(__dirname + '../public/ClientJS/GUI'));
+app.use("/js",  express.static(__dirname + '../public/ClientJS'));
+app.use("/img",  express.static(__dirname + '../public/img'));
+
 
 /* does this get moved into the createSimulation.js module too? */
 
