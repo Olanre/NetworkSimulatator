3d6ac602798989d056f7fcd4d5d199c076709714
Noah <Noah@computer>
2015-03-10 16:00:11 -0230

    more html

---
+++ NetworkSIM/public/ClientJS/HTML_Templates/WelcomePage.js
@@ -0,0 +1,4 @@
+function getWelcomePage(){
+	document.getElementById('title').innerHTML="<span id='welcome' class='fade-sideways-text'>Welcome</span>";
+	document.getElementById('container').innerHTML=""
+}
--- NetworkSIM/public/ClientJS/Views/ViewManager.js
+++ NetworkSIM/public/ClientJS/Views/ViewManager.js
@@ -2,9 +2,3 @@ window.onload = function(){
 	getWelcomePage();
 	loadStyleSheet('../css/defaultView.css');
 }
-
-
-function getWelcomePage(){
-	document.getElementById('title').innerHTML="<span id='welcome' class='fade-sideways-text'>Welcome</span>";
-	document.getElementById('container').innerHTML=""
-}
