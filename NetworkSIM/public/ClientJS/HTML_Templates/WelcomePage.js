8c999d643c83d307f1674281ae0fef0073d15a1b
Noah <Noah@computer>
2015-03-10 17:44:24 -0230

    working on css

--- NetworkSIM/public/ClientJS/HTML_Templates/WelcomePage.js
+++ NetworkSIM/public/ClientJS/HTML_Templates/WelcomePage.js
@@ -1,4 +1,8 @@
 function getWelcomePage(){
 	document.getElementById('title').innerHTML="<span id='welcome' class='fade-sideways-text'>Welcome</span>";
-	document.getElementById('container').innerHTML=""
+	document.getElementById('content').innerHTML="<div id='welcome-simulation-info'>"+
+		"<button id='create-simulation-button'>Create New Simulation</button>"+
+		"<br>"+
+		"<select size = '10' id ='join-simulation-area'></select>"+
+		"</div>";
 }
