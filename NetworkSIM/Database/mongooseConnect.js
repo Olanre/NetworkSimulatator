21bf1b3bea1fc1ea10ff9f958c953a1cec5e7117
Andrew <arc738@mun.ca>
2015-03-11 13:11:18 -0230

    zxsdfgb,mnfvxz

--- NetworkSIM/Database/mongooseConnect.js
+++ NetworkSIM/Database/mongooseConnect.js
@@ -16,26 +16,6 @@ Schema = db.Schema;
 
                                     //****Schemas
 
-//STATE - storing logs as a string
- var stateSchema = mongoose.Schema({
-	simulation_id : String,
-	state : [StateObject],
-});
-
-//State complete
-var State = mongoose.model('State', stateSchema, ' newStateSchema');
-	
-//State building block
-var stateObject = mongoose.Schema({
-   simulation : String,
-   timeStamp : String,
-});
-
-
-//state building block complete
-var StateObject = mongoose.model('StateObject', stateObject, 'newStateObject');
-								//event_queue_wrapper
-
 //mention to me if this needs testing, not sure if we are using it
 var event_queue_wrapperSchema = mongoose.Schema({
 	eventQueue: [String],
@@ -89,28 +69,7 @@ function findAllSimulations(callback)
 	});
 }
 	//fixed
-                        //USER FUNCTIONS**************!
-function addUser(aUser)
-{ 
-
-  var Lennete = new User(aUser);
-  Lennete.save();
-  //console.log("saved user " + aUser);
-}
-
-function modifyUser(aToken, aUser)
-{
-  User.findOne({token: aToken}, function(err, obj)
-  {
-  if(err) console.log("No user with that token");
-  console.log("user saved");
-  obj= aUser;
-  obj.save();
-  
-   //callback();
-	//console.log("User with token " + atoken + "edited " + example);	
-  });
-}
+                      
 //ACTIVITY CHANGES
 
 function updateUserAcitivity(aString, aUser, callback)
@@ -128,155 +87,10 @@ function updateUserAcitivity(aString, aUser, callback)
 		    	callback(); 
 		    	 });
 			});	
-	}
+}
 	
-//FETCH BY TOKEN 
-function getUserByToken(aToken, callback)
-{
-	//console.log(aToken);
-  User.findOne( {token: aToken}, function(err, obj)
-  { 
-    if(err) console.log("no user with token " + aToken);
-    
-	callback(obj);
-  });
-}
 
 
-                               //APP FUNCTIONS
-							   
-function addApp(anApp)
-{
-   var LenneteApp = new App(anApp);
-   LenneteApp.save();
-   //console.log("saved application " + anApp);
-}
 
-function getApp(callback)
-{
-   App.findOne( function(err, obj)
-   {
-	   callback(obj);
-});
-}
 
-function modifyApp(NewApp)
-{
-   App.findOne( function(err, obj)
-   {
-     obj = NewApp;
-     obj.save();
-     
-     //callback(LenneteApp);
-});
-}
                         
-//STATE FUNCTIONS
-
-function newState(aState)
-{
-  var LenneteState = new State(aState)
-  LenneteState.save();
-  //console.log("Saved state" + state);
-}
-
-//NETWORK FUNCTIONS
-function saveNetwork(aNetwork)
-{ 
-	var newNetwork = new Network(aNetwork);
-	newNetwork.save();
-}
-
-function getNetworkByName(aName, callback)
-{
-	Network.findOne( {network_name: aName}, function(err, obj)
-	{
-		if(err) console.log("no network with name: " + aName );
-		
-		console.log("found network" + obj);
-		callback(obj);
-	});
-}
-
-function modifyNetworkByName(aString, aNetwork)
-{
-	Network.findOne({network_name : aString}, function(err, obj)
-	{
-		if(err) console.log("No network with that name");
-		obj = aNetwork;
-	    obj.save();
-		console.log("Network saved");
-		//callback();
-	});
-}
-
-
-//Partition Methods
-function savePartition(aPartition)
-{
-	var newPartition = new Partition(aPartition);
-	newPartition.save();
-}
-
-function getPartitionByName(aName, callback)
-{
-	Partition.findOne({partition_name : aName}, function(err, obj)
-	{
-		if(err) console.log("No partition with name " + aName);
-		//console.log("found partition" + obj);
-		callback(obj);
-	});
-}
-
-
-function modifyPartitionByName(aString, aPartition)
-{
-   Partition.findOne({partition_name : aString}, function(err,obj)
-   {
-	   if(err) console.lgo("no Partition exists with that name");
-	   obj = aPartition;
-	   obj.save();
-	   console.log("partition saved");
-	   //callback();
-   });
-}
-
-//Global function
-
-function AllFieldsRequiredByDefautlt(schema) {
-    for (var i in schema.paths) {
-        var attribute = schema.paths[i]
-        if (attribute.isRequired == undefined) {
-            attribute.required(true);
-        }
-    }
-}
-
-
-
-
-
-
-
-//*Exports, finished calls
-module.exports.addUser = addUser;
-module.exports.getUserByToken = getUserByToken;
-module.exports.addSim = addSim;
-module.exports.modifyUser = modifyUser;
-module.exports.getSimByName = getSimByName;
-module.exports.getApp = getApp;
-module.exports.addApp = addApp;
-module.exports.modifyApp = modifyApp;
- 
- //New Since Refactor
-module.exports.modifySimByName = modifySimByName;
-module.exports.saveNetwork = saveNetwork;
-module.exports.modifyNetworkByName = modifyNetworkByName;
-module.exports.savePartition = savePartition;
-module.exports.modifyPartitionByName = modifyPartitionByName;
-module.exports.getPartitionByName = getPartitionByName;
-module.exports.getNetworkByName = getNetworkByName;
-
-
-module.exports.StateObject = StateObject;
-//module.exports.event_queue_wrapper = event_queue_wrapper;
