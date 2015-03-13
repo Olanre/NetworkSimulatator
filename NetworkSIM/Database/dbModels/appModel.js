87e2e824e4ea38454c04b8aedf7da64aabd71d5c
Andrew <arc738@mun.ca>
2015-03-13 12:38:12 -0330

    RDTMODEL appMODEL models.js updated with these models ready

--- NetworkSIM/Database/dbModels/appModel.js
+++ NetworkSIM/Database/dbModels/appModel.js
@@ -12,33 +12,6 @@ var applicationSchema = mongoose.Schema({
 	
 	});
 
-applicationSchema.statics.addApp = function (anApp)
-{
-   var LenneteApp = new App(anApp);
-   LenneteApp.save();
-   //console.log("saved application " + anApp);
-}
-
-applicationSchema.statics.getApp = function (callback)
-{
-   App.findOne( function(err, obj)
-   {
-	   callback(obj);
-   });
-}
-
-
-
-applicationSchema.statics.modifyApp = function (NewApp)
-{
-   App.findOne( function(err, obj)
-   {
-     obj = NewApp;
-     obj.save();
-     
-     //callback(LenneteApp);
-   });
-}
 
 
 module.exports = mongoose.model('App', applicationSchema, Apps);
