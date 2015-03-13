0da35465ca74a6de70df58ef3a7f32af01f914ba
Emily <emily.innes@live.com>
2015-03-13 11:39:31 -0230

    The client now receives the expected partition list.

--- NetworkSIM/tests/ModelTests/Utilitytest.js
+++ NetworkSIM/tests/ModelTests/Utilitytest.js
@@ -1,6 +1,28 @@
-var util=require("../public/ClientJS/Utilities.js");
-var badlist=[{name:'five',children:['test','test2']},{name:'am',children:['test','test2']},{name:'yo',children:['test8','test88']}];
-console.log(badlist[0].name);
-console.log(badlist[2].children[0]);
-var part=util.buildPartition(badlist);
-console.log(part);
+//var util=require("../public/ClientJS/Utilities.js");
+var Util=require("../../Utilities/utilities.js");
+
+function deepCopyTest(){
+
+	var stupidObject={
+			name: 'test',
+			vv:2,
+			a_list:[2,3,4
+			],
+			another_list:[
+				{name:'a'},{name:'b'},{name:'c'}
+			]
+		};
+
+	var newObject=Util.deepCopy(stupidObject);
+
+		stupidObject.vv=4;
+		newObject.another_list[0].name="changed";
+		stupidObject.a_list[1]='59';
+
+
+		var linked = stupidObject.vv===newObject.vv||stupidObject.another_list[0]===newObject.another_list[0]||stupidObject.a_list[1]===newObject.a_list[1];
+
+}
+
+
+deepCopyTest();
