f8e08a397cbad8d9ae11773ce655549fd11f457d
Emily-Innes <emily.innes@live.com>
2015-03-17 23:29:10 -0230

    Visual changes to Device List View and Network List View

--- NetworkSIM/public/ClientJS/HTML_Templates/AccountTemplate.js
+++ NetworkSIM/public/ClientJS/HTML_Templates/AccountTemplate.js
@@ -3,9 +3,19 @@
  * @returns str, the generated html string for the template
  */
 function AccountTemplate(device){
+	var deviceClone = deepCopy(device); 
+	var simulation = get_local_simulation();
+	for(index in simulation.partition_list){
+		var network = findByUniqueID(device.current_network, simulation.partition_list[index].network_list);
+		if(network!=-1){
+			deviceClone.current_network = network.network_name;
+			break;
+		}
+	}
+	
 	var template = document.getElementById('template12').innerHTML;
 	 textile = Hogan.compile(template);
-	 context = device;
+	 context = deviceClone;
 	 tpl = textile.render(context);
 	 return tpl;
 }
