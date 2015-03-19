c24ffe456b1d5c0d4b35c6ae23c1c436556fbdcb
Noah <Noah@192.168.0.11>
2015-03-19 15:13:29 -0230

    flippy text

--- NetworkSIM/public/ClientJS/HTML_Templates/SimulationListTemplate.js
+++ NetworkSIM/public/ClientJS/HTML_Templates/SimulationListTemplate.js
@@ -2,11 +2,12 @@ function SimulationDataListTemplate(simulations){
 	var local_device = get_local_device();
 	var str = '<h1 class="flip-up-text">List of current Simulations</h1>'+ "<div class = 'simulations_management'> ";
 	str += " <table id = 'simslist'> ";
+	str+="<tr><td>Simulation Name</td><td>Number of Devices</td><td>Number of Networks</td><td>Action</td></tr>";
 	if(simulations !== null && simulations !== undefined ){
 		for (var i =0 ; i < simulations.length; i++) {
 			 str += "	<tr> " + "<td> <div class = 'sim-name'>  " + simulations[i].simulation_name+ " </div> </td> " +
-				  "<td> <div class = 'num-devices'> Number of Devices: " + simulations[i].num_devices+ " </div> </td> " +
-				  "<td> <div class = 'num-networks'> Number of Networks: " + simulations[i].num_networks+ " </div> </td> ";
+				  "<td> <div class = 'num-devices'>" + simulations[i].num_devices+ " </div> </td> " +
+				  "<td> <div class = 'num-networks'>" + simulations[i].num_networks+ " </div> </td> ";
 			 		
 			 if(local_device.verified == true && local_device.current_simulation === simulations[i].simulation_id){
 				 str += "<td> <div  class = 'btn btn-primary'  onclick = 'SimulationManagementView()'> View </div> </td>";
