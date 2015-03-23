f642aec6e88dbc43470589ce720a78910098b298
Noah <Noah@10.23.10.135>
2015-03-23 13:23:39 -0230

    fixed history creating events

--- NetworkSIM/public/ClientJS/TopologyGUI/Manipulation.js
+++ NetworkSIM/public/ClientJS/TopologyGUI/Manipulation.js
@@ -28,23 +28,28 @@ interact('.device')
 	    },
 	    
 	    onstart: function(event){
-			//adds class to make sure object you are moving is on top 
-			var shape = getShapeFromEvent(event);
-			shape.element.classList.add('held-object');
+	    	if(interactable){
+				//adds class to make sure object you are moving is on top 
+				var shape = getShapeFromEvent(event);
+				shape.element.classList.add('held-object');
+			}
 		},
 
 		onmove: function (event) {
-		
-		//gets the circle from the list of shapes
-			var shape = getShapeFromEvent(event);
-			moveUIElementAndChildren(shape, event.dx,event.dy);
+			if(interactable){
+				//gets the circle from the list of shapes
+				var shape = getShapeFromEvent(event);
+				moveUIElementAndChildren(shape, event.dx,event.dy);
+			}
 		},
 		
 		onend: function(event){
-			//stops ensuring that object is ontop after dropped
-			var shape = getShapeFromEvent(event);
-			shape.element.classList.remove('held-object');
-			orderCanvas();
+			if(interactable){
+				//stops ensuring that object is ontop after dropped
+				var shape = getShapeFromEvent(event);
+				shape.element.classList.remove('held-object');
+				orderCanvas();
+			}
 		},
 	});
 
@@ -57,96 +62,100 @@ interact('.network')
 		overlap: 0.7,
 		
 		ondropactivate: function(event){
-			
-			event.target.classList.add('drop-locations');
+			if(interactable){
+				event.target.classList.add('drop-locations');
+			}
 		},
 		
 		ondragenter: function (event) {
-
-		    var draggableElement = event.relatedTarget,
-		        dropzoneElement = event.target;
-		    
-		    if (draggableElement.classList.contains('device')){
-			    dropzoneElement.classList.add('drop-target');
-			    draggableElement.classList.add('connected-device');
-		    }
+			if(interactable){
+			    var draggableElement = event.relatedTarget,
+			        dropzoneElement = event.target;
+			    
+			    if (draggableElement.classList.contains('device')){
+				    dropzoneElement.classList.add('drop-target');
+				    draggableElement.classList.add('connected-device');
+			    }
+			}
 		},
 
 		ondragleave: function (event) {
-			
-			  event.target.classList.remove('drop-target');
-			  event.relatedTarget.classList.remove('connected-device');
-			  var network=getShapeFromEvent(event);
-			  var networkIndex=event.target.getAttribute('data-index');
-			  var device=getRelatedShapeFromEvent(event);
-			  var deviceIndex=event.relatedTarget.getAttribute('data-index');
-			  delete network.children[deviceIndex];
-			  
+			if(interactable){
+				event.target.classList.remove('drop-target');
+				event.relatedTarget.classList.remove('connected-device');
+				var network=getShapeFromEvent(event);
+				var networkIndex=event.target.getAttribute('data-index');
+				var device=getRelatedShapeFromEvent(event);
+				var deviceIndex=event.relatedTarget.getAttribute('data-index');
+				delete network.children[deviceIndex];
+			}
 		},
 
 		
 		ondrop: function (event) {
-			var draggableElement=event.relatedTarget;
-			var dropzoneElement=event.target;
-			var dragClass=draggableElement.classList[0];
-			
-			var dropzone=getShapeFromEvent(event);
-			var dragged=getRelatedShapeFromEvent(event);
-			
-			if(dragClass==='device'){
-				attachChild(dropzone,dragged);
+			if(interactable){
+				var draggableElement=event.relatedTarget;
+				var dropzoneElement=event.target;
+				var dragClass=draggableElement.classList[0];
 				
-				deviceName=shapes[draggableElement.getAttribute('data-index')].name;
-				newNetworkName=shapes[dropzoneElement.getAttribute('data-index')].name;
+				var dropzone=getShapeFromEvent(event);
+				var dragged=getRelatedShapeFromEvent(event);
+				
+				if(dragClass==='device'){
+					attachChild(dropzone,dragged);
+					
+					deviceName=shapes[draggableElement.getAttribute('data-index')].name;
+					newNetworkName=shapes[dropzoneElement.getAttribute('data-index')].name;
 
-				moveDeviceToNetwork(dragged.represents.token,dropzone.represents._id);
-				
+					moveDeviceToNetwork(dragged.represents.token,dropzone.represents._id);
+				}
 			}
 			if(dragClass==='network'){
-				var circleDragging = shapes[draggableElement.getAttribute('data-index')];
-				var circleDraggedTo = shapes[dropzoneElement.getAttribute('data-index')];
-				
-				if(!partitionExists(dropzone,dragged)){
-
-					var partition=createPartitionGraphic(dropzone,dragged);
-					var partitionA = findPartitionIDForNetwork(dropzone.represents);
-					var partitionB = findPartitionIDForNetwork(dragged.represents);
-					if(partitionA!=partitionB) mergePartition(partitionA,partitionB);
+				if(interactable){
+					var circleDragging = shapes[draggableElement.getAttribute('data-index')];
+					var circleDraggedTo = shapes[dropzoneElement.getAttribute('data-index')];
 					
+					if(!partitionExists(dropzone,dragged)){
 
-				}
-				else{
-					var oldpartitionID=findPartitionIDForNetwork(dragged.represents);
+						var partition=createPartitionGraphic(dropzone,dragged);
+						var partitionA = findPartitionIDForNetwork(dropzone.represents);
+						var partitionB = findPartitionIDForNetwork(dragged.represents);
+						if(partitionA!=partitionB) mergePartition(partitionA,partitionB);
+					}
+					else{
+						var oldpartitionID=findPartitionIDForNetwork(dragged.represents);
 
-					removePartition(dropzone,dragged);
+						removePartition(dropzone,dragged);
 
-					var newpartitionlist=breadthFirstSearch(dropzone,dragged);
-					var connected=false;
-					for(partition in newpartitionlist){
-						if(newpartitionlist[partition]==dragged){
-							connected=true;
+						var newpartitionlist=breadthFirstSearch(dropzone,dragged);
+						var connected=false;
+						for(partition in newpartitionlist){
+							if(newpartitionlist[partition]==dragged){
+								connected=true;
+							}
+						}
+
+						if(!connected){
+							var list = [];
+							for(var i=0;i<newpartitionlist.length;i++){
+								list.push(newpartitionlist[i].represents);
+							}
+							dividePartition(list,oldpartitionID);
 						}
 					}
-
-					if(!connected){
-						var list = [];
-						for(var i=0;i<newpartitionlist.length;i++){
-							list.push(newpartitionlist[i].represents);
-						}
-						dividePartition(list,oldpartitionID);
-					}
+					snapToLocation(dragged,origin);
 				}
-				snapToLocation(dragged,origin);
 			}
 		
 		},
 
 		ondropdeactivate: function (event) {
-			
-			event.target.classList.remove('drop-locations');
-		    event.target.classList.remove('drop-target');
-		    //HERE SHOULD BE WHERE YOU TEST IF YOU DROP THIS INTO
-		    //THE FREE LIST
+			if(interactable){
+				event.target.classList.remove('drop-locations');
+			    event.target.classList.remove('drop-target');
+			    //HERE SHOULD BE WHERE YOU TEST IF YOU DROP THIS INTO
+			    //THE FREE LIST
+			}
 		},
 	})
 	
@@ -162,25 +171,31 @@ interact('.network')
 		    },
 		
 			onmove: function (event) {
-				var shape = getShapeFromEvent(event);
-				moveUIElementAndChildren(shape,event.dx,event.dy);
-				updatePartitionLines(shape);
+				if(interactable){
+					var shape = getShapeFromEvent(event);
+					moveUIElementAndChildren(shape,event.dx,event.dy);
+					updatePartitionLines(shape);
+				}
 			},
 			
 			onstart: function(event){
-		    	var shape = getShapeFromEvent(event);
-				origin.x=shape.x;
-				origin.y=shape.y;
-				
-				//adds class to make sure object you are moving is on top 
-				var shape = getShapeFromEvent(event);
-				shape.element.classList.add('held-object');
+				if(interactable){
+			    	var shape = getShapeFromEvent(event);
+					origin.x=shape.x;
+					origin.y=shape.y;
+					
+					//adds class to make sure object you are moving is on top 
+					var shape = getShapeFromEvent(event);
+					shape.element.classList.add('held-object');
+				}
 			},
 			onend: function(event){
-				//stops ensuring that object is ontop after dropped
-				var shape = getShapeFromEvent(event);
-				shape.element.classList.remove('held-object');
-				orderCanvas();
+				if(interactable){
+					//stops ensuring that object is ontop after dropped
+					var shape = getShapeFromEvent(event);
+					shape.element.classList.remove('held-object');
+					orderCanvas();
+				}
 			},
 		});
 
@@ -227,16 +242,14 @@ function hasClass(element, Elclass) {
 }
 
 function moveUIElementAndChildren(UIShape,dx,dy){
-	if(interactable==true){
-		UIShape.x +=dx;
-		UIShape.y += dy;
-		for(index in UIShape.children){
-			UIShape.children[index].x+=dx;
-			UIShape.children[index].y+=dy;
-			UIShape.children[index].draw();
-		}
-		UIShape.draw();
+	UIShape.x +=dx;
+	UIShape.y += dy;
+	for(index in UIShape.children){
+		UIShape.children[index].x+=dx;
+		UIShape.children[index].y+=dy;
+		UIShape.children[index].draw();
 	}
+	UIShape.draw();
 }
 
 function partitionExists(dropzoneObject, dragObject){
